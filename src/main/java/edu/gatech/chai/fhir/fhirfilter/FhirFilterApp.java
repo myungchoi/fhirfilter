638cc5f5cf663f4888b244e52eb4c3d704d8ec34
Myung Choi <myungchoi@gmail.com>
2018-11-20 13:46:57 -0500

    tomcat runnable update

--- src/main/java/edu/gatech/chai/fhir/fhirfilter/FhirFilterApp.java
+++ src/main/java/edu/gatech/chai/fhir/fhirfilter/FhirFilterApp.java
@@ -2,14 +2,14 @@ package edu.gatech.chai.fhir.fhirfilter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 /**
  * 
  */
 
 @SpringBootApplication
-public class FhirFilterApp 
-{
+public class FhirFilterApp extends SpringBootServletInitializer {
     public static void main( String[] args )
     {
         SpringApplication.run(FhirFilterApp.class, args);
